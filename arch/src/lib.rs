54dc83967aac8ffd50977893a36c7f15258fcda4
SforSam <ItsSforSam@users.noreply.github.com>
2026-04-17 14:52:15 -0500

    Clean up arch crate

--- arch/src/lib.rs
+++ arch/src/lib.rs
@@ -1,12 +1,17 @@
 // This re-exports all the architecture specific
 #![no_std]
 
-#[cfg(target_arch = "x86_64")]
-pub use oes_arch_x86 as x86;
+cfg_if::cfg_if! {
+    if #[cfg(target_arch = "x86_64")]{
+        pub use oes_arch_x86 as x86;
+        pub use oes_arch_x86 as current;
+    }else {
+        compile_error!("Architecture ",env!("CARGO_CFG_TARGET_ARCH"), "not supported.");
+    }
+}
 
-
-pub mod common{
-    #[expect(unused_imports)]
+pub mod common {
     #[doc(inline)]
+    #[allow(unused_imports)]
     pub use ::oes_arch_common::*;
 }
