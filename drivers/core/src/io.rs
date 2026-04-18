933aca54e08f8058498f9f6ec97fc28aea7003ff
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2026-04-18 14:51:23 -0500

    [Ticket: X]

---
+++ drivers/core/src/io.rs
@@ -0,0 +1,99 @@
+//! Provides traits for dealing with IO
+//!
+//! This allows an easy Hardware abstraction layer
+use core::fmt;
+pub trait Write {
+    /// Write a buffer into the writer
+    ///
+    /// # Returns
+    /// [`Some(n)`] is how many bytes where written
+    ///
+    /// [`None`] is an error occured
+    ///
+    /// **Note, this is pained to return a [`Result<T,E>`] in the future**
+    ///
+    /// [`Some(n)`]: Some
+    // TODO: make a generic error type
+    fn write(&mut self, buf: &[u8]) -> Option<usize>;
+    /// Flush from internal buffer into IO object
+    ///
+    /// # Returns
+    /// [`Some(())`] on success
+    ///
+    /// [`None`] when a error occurred.
+    ///
+    /// **Note, this is pained to return a [`Result<T,E>`] in the future**
+    ///
+    /// [`Some(())`]: Some
+    fn flush(&mut self) -> Option<()>;
+    /// Attempts to write the whole buffer into the writer
+    ///
+    /// # Returns
+    /// [`Some(())`] on success
+    ///
+    /// [`None`] when a error occurred.
+    ///
+    /// **Note, this is pained to return a [`Result<T,E>`] in the future**
+    /// [`Some(())`]: Some
+    fn write_all(&mut self, mut buf: &[u8]) -> Option<()> {
+        // TODO: once proper error type is made, check if it's
+        // because it was interrupted
+        while !buf.is_empty() {
+            match self.write(buf) {
+                Some(0) => return None,
+                Some(n) => buf = &buf[n..],
+                None => return None,
+            }
+        }
+        Some(())
+    }
+    /// # Returns
+    /// [`Some(())`] on success
+    ///
+    /// [`None`] when a error occurred.
+    ///
+    /// **Note, this is pained to return a [`Result<T,E>`] in the future**
+    /// [`Some(())`]: Some
+    fn write_fmt(&mut self, args: fmt::Arguments<'_>) -> Option<()> {
+        if let Some(s) = args.as_str() {
+            self.write_all(s.as_bytes())
+        } else {
+            todo!();
+        }
+    }
+}
+
+// fn default_write_fmt<W:Write+?Sized+fmt::Write>(this:&mut W,args: fmt::Arguments)->Option<()>{
+
+//     match fmt::write(&*this,args){
+//         Ok(()) => Some(()),
+//         Err(_) => None
+//     }
+// }
+
+impl fmt::Write for dyn Write + '_ {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        match Write::write_all(self, s.as_bytes()) {
+            None => Err(fmt::Error),
+            Some(_) => Ok(()),
+        }
+    }
+}
+#[doc(alias = "empty")]
+pub const fn no_op() -> NoOp {
+    NoOp
+}
+
+#[doc(alias = "Empty")]
+#[non_exhaustive]
+pub struct NoOp;
+
+impl Write for NoOp {
+    fn write(&mut self, buf: &[u8]) -> Option<usize> {
+        Some(buf.len())
+    }
+
+    fn flush(&mut self) -> Option<()> {
+        Some(())
+    }
+}
--- drivers/core/src/lib.rs
+++ drivers/core/src/lib.rs
@@ -3,13 +3,14 @@
 #![feature(debug_closure_helpers)]
 use core::{
     cell::UnsafeCell,
-    fmt::{self, Debug},
+    fmt::Debug,
     marker::{PhantomData, PhantomPinned},
     mem::MaybeUninit,
     ptr::NonNull,
 };
 pub mod error;
 pub mod generic;
+pub mod io;
 
 // use downcast_rs::{Downcast, impl_downcast};
 /// Generic Driver trait
@@ -196,84 +197,6 @@ impl Default for DriverMemory<'_> {
             size: 0,
             is_rox: true,
             _data: PhantomData,
-        }
-    }
-}
-
-pub trait Write {
-    /// Write a buffer into the writer
-    ///
-    /// # Returns
-    /// [`Some(n)`] is how many bytes where written
-    ///
-    /// [`None`] is an error occured
-    ///
-    /// **Note, this is pained to return a [`Result<T,E>`] in the future**
-    ///
-    /// [`Some(n)`]: Some
-    // TODO: make a generic error type
-    fn write(&mut self, buf: &[u8]) -> Option<usize>;
-    /// Flush from internal buffer into IO object
-    ///
-    /// # Returns
-    /// [`Some(())`] on success
-    ///
-    /// [`None`] when a error occurred.
-    ///
-    /// **Note, this is pained to return a [`Result<T,E>`] in the future**
-    ///
-    /// [`Some(())`]: Some
-    fn flush(&mut self) -> Option<()>;
-    /// Attempts to write the whole buffer into the writer
-    ///
-    /// # Returns
-    /// [`Some(())`] on success
-    ///
-    /// [`None`] when a error occurred.
-    ///
-    /// **Note, this is pained to return a [`Result<T,E>`] in the future**
-    /// [`Some(())`]: Some
-    fn write_all(&mut self, mut buf: &[u8]) -> Option<()> {
-        // TODO: once proper error type is made, check if it's
-        // because it was interrupted
-        while !buf.is_empty() {
-            match self.write(buf) {
-                Some(0) => return None,
-                Some(n) => buf = &buf[n..],
-                None => return None,
-            }
-        }
-        Some(())
-    }
-    /// # Returns
-    /// [`Some(())`] on success
-    ///
-    /// [`None`] when a error occurred.
-    ///
-    /// **Note, this is pained to return a [`Result<T,E>`] in the future**
-    /// [`Some(())`]: Some
-    fn write_fmt(&mut self, args: fmt::Arguments<'_>) -> Option<()> {
-        if let Some(s) = args.as_str() {
-            self.write_all(s.as_bytes())
-        } else {
-            todo!();
-        }
-    }
-}
-
-// fn default_write_fmt<W:Write+?Sized+fmt::Write>(this:&mut W,args: fmt::Arguments)->Option<()>{
-
-//     match fmt::write(&*this,args){
-//         Ok(()) => Some(()),
-//         Err(_) => None
-//     }
-// }
-
-impl fmt::Write for dyn crate::Write + '_ {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        match Write::write_all(self, s.as_bytes()) {
-            None => Err(fmt::Error),
-            Some(_) => Ok(()),
         }
     }
 }
