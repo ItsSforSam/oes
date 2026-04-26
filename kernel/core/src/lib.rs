599bf70750d392245cb7bf4e1c635ebdf9c64062
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2026-04-25 23:15:21 -0500

    Add abort helper and sched  module

--- kernel/core/src/lib.rs
+++ kernel/core/src/lib.rs
@@ -1,6 +1,32 @@
+#![feature(negative_impls)]
+#![feature(allocator_api)]
+#![feature(linkage)]
+#![feature(core_intrinsics)]
+#![expect(internal_features, reason = "for core::intrinsics::abort")]
 #![no_std]
 pub mod printk;
+pub mod sched;
 pub mod sync;
 
+// #[cfg(feature = "alloc")] // We use it regardless
+extern crate alloc as liballoc;
 #[cfg(feature = "alloc")]
-extern crate alloc;
+pub mod alloc;
+
+pub mod prelude {
+    #[cfg(feature = "alloc")]
+    pub use liballoc::boxed::Box;
+}
+/// This will abort the kernel. Shut off the system, no questions
+///
+/// # SAFETY
+/// This function doesn't cause any issues of it's own
+/// but this can cause corruption with file systems with partially written data.
+///
+///
+/// Each architecture should provide a dedicated
+#[linkage = "weak"]
+#[unsafe(no_mangle)]
+pub unsafe fn abort() -> ! {
+    core::intrinsics::abort()
+}
---
+++ kernel/core/src/sched/mod.rs
@@ -0,0 +1,6 @@
+//! Handles the scheduler logic
+
+/// A Task can be anything
+///
+/// A user process, a kernel thread
+pub struct Task {}
