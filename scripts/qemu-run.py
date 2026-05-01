a945c93a3bb866e28d96b635ebf8c850331d1a24
SforSam <ItsSforSam@users.noreply.github.com>
2026-05-01 14:49:13 -0500

    More work on qemu-run.py
    Not done, just more work done

--- scripts/qemu-run.py
+++ scripts/qemu-run.py
@@ -3,38 +3,114 @@ from __future__ import annotations
 import argparse
 import typing as t
 from pathlib import Path
+import subprocess
+import contextlib
+import shutil
+import os
+
+bin = Path("./bin")
 def main():
+    with contextlib.suppress(FileExistsError):
+        os.mkdir(bin)
     p = _get_parser()
+    args = p.parse_args()
+
+    if args.build:
+        compile(args.target)
+    
 
 
 def _get_parser()->argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.suggest_on_error = True # If available, enable
-
-    parser.add_argument(
+    exclusive = parser.add_mutually_exclusive_group()
+    exclusive.add_argument(
         *[
             "--path"
         ],
-        help="The path of the compiled binary"
+        help="The path of the compiled binary",
+        type=Path,
+        required=True,
 # metavar="triple",
         type=Path
     )
+    exclusive.add_argument(
+        *[
+            "--build",
+        ],
+        help="Have this program auto build",
+        action="store_true",
+        default=False,
+    )
     parser.add_argument(
-        *[""
+        *[
         "--target"
         ],
         help="The target triple",
-        metavar="TARGET-TRIPLE"
+        metavar="TARGET-TRIPLE",
+        type=TargetTriple,
+        required=True,
+        default=TargetTriple("x86_64-unknown-none")
     )
-
+    
 
     return parser
+
+
+def compile(triple:"TargetTriple"):
+    # import subprocess
+    args = ["cargo", "build"]
+    args.append(f"--target={triple}")
+    if triple.is_uefi:
+        args.append("--features=uefi")
+    proc:subprocess.CompletedProcess[str]
+    try:
+        proc = subprocess.run(
+        *args,check=True
+        )
+    except subprocess.CalledProcessError as e:
+        raise CompilerFailedError("RUSTC has failed. Either this is a us bug or rust bug.") from e
+    
+def image(kernel_bin:Path):
+    """
+    This will create a boot image
+    """
+    
+    
+    # UEFI firmwhere
+    shutil.copyfile("/usr/share/OVMF/OVMF_CODE.fd",bin)
+    shutil.copyfile("/usr/share/OVMF/OVMF_VARS.fd",bin)
+
+    dfile = bin / "disk.dd"
+    subprocess.run( # create an empty disk image file, 128 megabytes in size. 
+        *["dd","if=/dev/zero",f"of={dfile}", "bs=1048576", "count=128"],
+        check=True
+    )
+    losetup_cmd = [f"losetup -o {2048*512} --sizelimit {8*1024*1024} --show -f ".split(),f"{dfile}"]
+    print("We need you to enter your password to allow loop devices")
+    e = subprocess.run(
+        *losetup_cmd,
+    )
+    if e.returncode != 0 and "Permission denied" in e.stderr:
+        # We use pkexec instead of sudo due to two things
+        # 1. We don't need to interact with passwords
+        # 2. We don't need to use `sudo -S`
+
+        e = subprocess.run(
+            ["pkexec" "--keep-cwd", *losetup_cmd],
+            check=True
+        )
 
 class TargetTriple:
     __arch:str
     __is_uefi:bool
     def __init__(self,v:str):
-        ...
+        t= v.split("-")
+        if len(t) < 3:
+            raise ValueError(f"Malformed Target triple {v!r}")
+        self.__arch = t[0]
+        self.__is_uefi = True if t[-1] == "uefi" else False
+    
     def __repr__(self) -> str:
         v = "uefi" if self.__is_uefi else "none"
         arch = self.__arch
@@ -56,6 +132,15 @@ class TargetTriple:
     def is_uefi(self)->bool:
         # don't need to use a setter
         return self.__is_uefi 
+
+
+class CompilerFailedError(RuntimeError):
+    """
+    Used with `--build` flag
+
+    The compiler has failed
+    """
+    ...
 
 
 if __name__ == "__main__":
