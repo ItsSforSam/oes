a5536b6e3f026ed1504b6421114c12b3d39f887d
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2026-05-03 19:19:01 -0500

    Make some bare shell of schedule.
    Adds comments and a yield_now function

--- kernel/core/src/sched/mod.rs
+++ kernel/core/src/sched/mod.rs
@@ -3,4 +3,29 @@
 /// A Task can be anything
 ///
 /// A user process, a kernel thread
-pub struct Task {}
+pub struct Task(());
+
+/// Yields the current thread
+///
+/// This is great if you are waiting on data from a another
+/// thread, although take a look at the [`sync`] module
+/// for synchronization primitives, which may better suit your
+/// needs
+///
+/// [`sync`]: crate::sync
+pub fn yield_now() {
+    // @TODO: make scheduler
+    // Not using todo!(), as to avoid panicking and due to
+    // us not having a scheduler, and this functioning as a no-op
+    // Something that may be helpful that if no other tasks are needed,
+    // either use core::hint::spin_loop() or use hlt (or architecture
+    // equivalent)
+    //
+    // Side note, which should have some discussion, is that if
+    // there should be some sort of fast pass to this function
+    //
+    // Thought should be if there should be of sort a custom
+    // interrupt, similar to unix-like's int 0x80 for syscalls.
+    // This is due to the nature of yielding
+    core::hint::spin_loop();
+}
