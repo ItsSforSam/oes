98b486c8b1a9fb7e9548101fc1f19e2e1fc251ed
SforSam <ItsSforSam@users.noreply.github.com>
2026-05-13 14:05:36 -0500

    Have our yield_now function use miri's yield function if available

--- kernel/core/src/sched/mod.rs
+++ kernel/core/src/sched/mod.rs
@@ -14,6 +14,11 @@ pub struct Task(());
 ///
 /// [`sync`]: crate::sync
 pub fn yield_now() {
+    cfg_if::cfg_if! {
+        if #[cfg(miri)]{
+        oes_arch::common::miri::miri_yield();
+
+        }else {
     // @TODO: make scheduler
     // Not using todo!(), as to avoid panicking and due to
     // us not having a scheduler, and this functioning as a no-op
@@ -27,5 +32,10 @@ pub fn yield_now() {
     // Thought should be if there should be of sort a custom
     // interrupt, similar to unix-like's int 0x80 for syscalls.
     // This is due to the nature of yielding
+    // BUT this would have to be a commitment and considered if it's even worth
+    // the cost, at-least on x86_64, the sysenter/sysexit is designed
+    // for transering to and from userspace.
     core::hint::spin_loop();
+        }
+    }
 }
