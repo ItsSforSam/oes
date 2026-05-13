a592b821659aa4d458c86a99d96fc13a2a5e7cac
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2026-05-13 09:04:33 -0500

    Fix oes-macros mod name

--- kernel/macros/src/lib.rs
+++ kernel/macros/src/lib.rs
@@ -1,4 +1,4 @@
-mod impls;
+mod syscall;
 
 extern crate proc_macro;
 extern crate self as oes_kernel_core;
@@ -29,11 +29,11 @@ pub fn syscall(
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
     let item = syn::parse_macro_input!(item as syn::ItemFn);
-    match impls::check_syscall_sig(&item) {
+    match syscall::check_syscall_sig(&item) {
         Ok(_) => { /* Nothing */ }
         Err(e) => {
             e.into_compile_error();
         }
     };
-    impls::syscall_impl(item).into()
+    syscall::syscall_impl(item).into()
 }
