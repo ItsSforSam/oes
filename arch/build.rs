12613931f10b08e9d118d5bab791652aa49cf5db
SforSam <ItsSforSam@users.noreply.github.com>
2026-05-19 14:44:57 -0500

    Expand build.rs

--- arch/build.rs
+++ arch/build.rs
@@ -5,10 +5,38 @@ fn main() {
     let arch = TargetArchitecture::from_str(&arch).unwrap();
     println!("cargo::rustc-link-arg=-T{arch}/ld-oes.ld");
 }
+/// Represents all available targets for The Odyssey Entertainment System.
 #[expect(nonstandard_style, reason = "Doesn't read well in context")]
+#[derive(Debug, PartialEq, Eq)]
+#[non_exhaustive]
 pub enum TargetArchitecture {
     /// Represents x86 and x86_64
     x86,
+}
+impl Default for TargetArchitecture {
+    /// The = is [`x86`] currently, besides that being our only supported target,
+    /// this is due to it being the most supported target, but that can always change, so don't rely on
+    /// it always being [`x86`]. If you need it to be [`x86`], just use it explicitly
+    ///
+    /// # Example
+    ///
+    /// Using a sain default target
+    /// ```
+    /// # fn get_current_target() -> Option<TargetArchitecture> {None}
+    ///
+    /// let current_target: TargetArchitecture = get_current_target().unwrap_or_default();
+    /// ```
+    /// What not to do
+    /// ```no_run
+    /// let definitely_x86:TargetArchitecture = Default::default();
+    /// // This doesn't panic, but can start panicking with little to no notice
+    /// assert!(definitely_x86 == TargetArchitecture::x86 )
+    /// ```
+    ///
+    /// [`x86`]: TargetArchitecture::x86
+    fn default() -> Self {
+        TargetArchitecture::x86
+    }
 }
 #[derive(Debug)]
 pub struct UnsupportedArchitecture(String);
